f75ca3740aa4bf6126d8ae08b4434d817c173582
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-10-23 20:23:28 +0300

    ADD: Ex.3.16

--- index.js
+++ index.js
@@ -70,10 +70,14 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+  Person
+    .findOneAndDelete({ _id: req.params.id })
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      res.status(400).send({ error: 'malformatted id'})
+    })
 })
 
 app.post('/api/persons', (req, res) => {
