ea3425ad663623b45b68aef0b730d17cc9da5c68
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-10-24 12:56:22 +0300

    ADD: Ex.3.21
    Separate the production and development databases

--- models/person.js
+++ models/person.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
 const url = process.env.MONGODB_URI
+
 mongoose.set('useFindAndModify', false)
 
 mongoose.connect(url, {useNewUrlParser: true})
