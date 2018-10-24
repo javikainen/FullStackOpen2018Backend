aa464e06742ef7b929dc33847358cde821454dbf
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-10-24 12:12:20 +0300

    ENH: Use environment variable for the database address

--- models/person.js
+++ models/person.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://user:passwd@ds139883.mlab.com:39883/fullstack-phonebook'
+const url = process.env.MONGODB_URI
 mongoose.set('useFindAndModify', false)
-
 
 mongoose.connect(url, {useNewUrlParser: true})
 
